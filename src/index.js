6ce324dc7fc604b32905416756a01eab5d2de464
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2022-11-16 00:50:53 +0700

    Menyempurnakan program timer left

--- src/index.js
+++ src/index.js
@@ -69,6 +69,74 @@ const toggleMode=()=>{
 		}
 }
 
+const gameOver = (playerTurn,movesLeft) => {
+	const chooseMove = document.querySelector('.move');
+	const result = document.querySelector('.result');
+	const resetButton = document.querySelector('#reset');
+	const backButton = document.querySelector('#mainMenu');
+	const timeLeft = document.querySelector('#countdown');
+	stop();	
+	// menghilangkan setiap tombol button hand, tampilan Move Left sisa dan waktu countdown
+	playerTurn.forEach(action => {
+		action.style.display = 'none';
+	})
+	movesLeft.style.display = 'none';
+	timeLeft.style.display = 'none';
+
+	// Menampilkan game over
+	chooseMove.innerText = 'Game Over!!'
+	// Memperbesar ukuran font hasil akhir
+	result.style.fontSize = '2rem';
+
+	// Kalkulasi dan aksi terhadap hasil akhir
+	if(playerScore > botScore){
+		result.innerText = 'You are the winner !!!'
+	}
+	else if(playerScore < botScore){
+		result.innerText = 'Bot is the winner !!';
+	}
+	else{
+		result.innerText = 'Draw match';
+	}
+	// menampilkan tombol reset dan back to menu utama
+	resetButton.style.display = 'flex';
+	backButton.style.display = 'flex';
+
+	//aksi tombol reset
+	resetButton.addEventListener('click',()=>{
+		const playerScoreBoard = document.querySelector('.playerPoint');
+		const botScoreBoard = document.querySelector('.botPoint');
+		const playerHand = document.querySelector('.player-hand');
+		const botHand = document.querySelector('.bot-hand');
+		playerTurn.forEach(action => {
+			action.style.display = 'flex';
+		})
+		chooseMove.innerText = 'Choose Your Attempt!!'
+
+		resetButton.style.display = 'none';
+		backButton.style.display = 'none';
+		playerScore = 0;
+		botScore = 0;
+		moves = 5;
+		result.style.fontSize = '1.2rem';
+		movesLeft.style.display = 'flex';
+		timeLeft.style.display = 'flex';
+		movesLeft.innerText = `Move Left : ${moves}`;
+		document.getElementById("countdown").innerText = `Timer Left : 10 s`;
+		result.innerText = '';
+		playerHand.src = `src/img/rockmatch.png`;
+		botHand.src = `src/img/rockmatch.png`;
+		botScoreBoard.textContent = botScore;
+		playerScoreBoard.textContent = playerScore;
+		start();
+})
+		//Belum selesai sebenarnya
+	backButton.addEventListener('click',function(){
+		window.location.reload();
+	})
+}
+
+
 //tampilan menu utama 	
 const menu = () => {
 	const playBtn = document.querySelector("#playButton");
@@ -88,22 +156,33 @@ const menu = () => {
 var timer;
 var timeLeft;
 const start = () =>{
+	var value = true
 	const result = document.querySelector('.result');
 	const botScoreBoard = document.querySelector('.botPoint');
 	const movesLeft = document.querySelector('.movesleft');
-
+	const rockButton = document.querySelector('#rock');
+	const paperButton = document.querySelector('#paper');
+	const scissorButton = document.querySelector('#scissor');
+	const playerTurn = [rockButton,paperButton,scissorButton];
 	timeLeft = 10;
-	timer = setInterval(function(){
-		document.getElementById("countdown").innerText = `Timer Left: ${timeLeft} s`;
+	timer = setInterval(()=>{
+		document.getElementById("countdown").innerText = `Timer Left : ${timeLeft} s`;
 		if (timeLeft == 0){
-			clearInterval(timer);
+			stop();
 			moves--;
 			movesLeft.innerText = `Move Left : ${moves}`;
+			document.getElementById("countdown").innerText = `Times Up !!!`;
 			botScore++;
 			botScoreBoard.textContent = botScore;
 			result.textContent = 'You Lose';
+			if(moves == 0){
+				gameOver(playerTurn,movesLeft);
+				value = false;
+			}
+			if(value == true){
+			start();
+			}
 		}
-		
 	timeLeft--;
 	}, 1000);
 }
@@ -147,7 +226,7 @@ const game = () => {
 				})
 				playerHand.src = `src/img/rockmatch.png`;
                 botHand.src = `src/img/rockmatch.png`;
-				
+				// proses setelah animasi
 				setTimeout(() => {
 					// call fungsi untuk cek pemenangnya siapa
 					winner(this.innerText,botChoice);
@@ -158,12 +237,12 @@ const game = () => {
 					playerHand.src = `src/img/${this.innerText}match.png`;
                 	botHand.src = `src/img/${botChoice}match.png`;
 					// Deklarasi kalau jatah giliran sudah habis
-					start();
+					// start();
 					if(moves == 0){
 						gameOver(playerTurn,movesLeft);
 					}
 				}, 2000);
-				//Working on animation
+				//proses selama animasi 
 				stop();
 				playerHand.style.animation = 'shakePlayer 2s ease';
 				botHand.style.animation = 'shakeBot 2s ease';
@@ -181,6 +260,7 @@ const game = () => {
 		const result = document.querySelector('.result');
 		const playerScoreBoard = document.querySelector('.playerPoint');
 		const botScoreBoard = document.querySelector('.botPoint');
+		start();
 		//Kasus Tie
 		if(player == bot){
 			result.textContent = 'Tie'
@@ -201,72 +281,6 @@ const game = () => {
 		}
 	}
 		
-	// Giliran udah limit
-	const gameOver = (playerTurn,movesLeft) => {
-		const chooseMove = document.querySelector('.move');
-		const result = document.querySelector('.result');
-		const resetButton = document.querySelector('#reset');
-		const backButton = document.querySelector('#mainMenu');
-		const timeLeft = document.querySelector('#countdown');
-		// menghilangkan setiap tombol button hand, tampilan Move Left sisa dan waktu countdown
-		playerTurn.forEach(action => {
-			action.style.display = 'none';
-		})
-		movesLeft.style.display = 'none';
-		timeLeft.style.display = 'none';
-	
-		// Menampilkan game over
-		chooseMove.innerText = 'Game Over!!'
-		// Memperbesar ukuran font hasil akhir
-		result.style.fontSize = '2rem';
-
-		// Kalkulasi dan aksi terhadap hasil akhir
-		if(playerScore > botScore){
-			result.innerText = 'Player Win !!!'
-		}
-		else if(playerScore < botScore){
-			result.innerText = 'Bot Win !!';
-		}
-		else{
-			result.innerText = 'Tie';
-		}
-		// menampilkan tombol reset dan back to menu utama
-		resetButton.style.display = 'flex';
-		backButton.style.display = 'flex';
-
-		//aksi tombol reset
-		resetButton.addEventListener('click',()=>{
-			const playerScoreBoard = document.querySelector('.playerPoint');
-			const botScoreBoard = document.querySelector('.botPoint');
-			const playerHand = document.querySelector('.player-hand');
-			const botHand = document.querySelector('.bot-hand');
-			playerTurn.forEach(action => {
-				action.style.display = 'flex';
-			})
-			chooseMove.innerText = 'Choose Your Attempt!!'
-
-			resetButton.style.display = 'none';
-			backButton.style.display = 'none';
-			playerScore = 0;
-			botScore = 0;
-			moves = 5;
-			result.style.fontSize = '1.2rem';
-			movesLeft.style.display = 'flex';
-			timeLeft.style.display = 'flex';
-
-			result.innerText = '';
-			playerHand.src = `src/img/rockmatch.png`;
-            botHand.src = `src/img/rockmatch.png`;
-			botScoreBoard.textContent = botScore;
-			playerScoreBoard.textContent = playerScore;
-	})
-			//Belum selesai sebenarnya
-		backButton.addEventListener('click',function(){
-			window.location.reload();
-		})
-	}
-
-
 	// Mulai Game
 	playGame();
 	
